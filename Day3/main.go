fe5f20f8c1dee31134b519eb4a91d266424a4dda
Shabinn-k <shhabin.07@gmail.com>
2026-06-18 10:50:40 +0530

    practice

--- Day3/main.go
+++ Day3/main.go
@@ -98,58 +98,58 @@
 // }
 
 //DELETING AT BEGINNING
-// package main
-// import f "fmt"
-// type Node struct{
-// 	data int
-// 	next *Node
-// }
-// func main() {
-// 	node1:=&Node{data: 1}
-// 	node2:=&Node{data: 2}
-// 	node3:=&Node{data: 3}
-
-// 	node1.next=node2
-// 	node2.next=node3
-
-// 	head:=node1
-// 	head=head.next
-
-// 	for head!=nil{
-// 		f.Println(head.data)
-// 		head=head.next
-// 	}
-// }
-
 package main
-import "fmt"
-type Node struct {
+import f "fmt"
+type Node struct{
 	data int
 	next *Node
 }
-
 func main() {
-	node1 := &Node{data: 10}
-	node2 := &Node{data: 20}
-	node3 := &Node{data: 30}
+	node1:=&Node{data: 1}
+	node2:=&Node{data: 2}
+	node3:=&Node{data: 3}
 
-	node1.next = node2
-	node2.next = node3
+	node1.next=node2
+	node2.next=node3
 
-	head := node1
-	current := head
-	target:=20
+	head:=node1
+	head=head.next
 
-	for current != nil {
-		if current.data==target{
-			fmt.Println("value found",current.data)
-			return
-		}
-		current=current.next
+	for head!=nil{
+		f.Println(head.data)
+		head=head.next
 	}
-	fmt.Println("value not found")
-
 }
+
+// package main
+// import "fmt"
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// func main() {
+// 	node1 := &Node{data: 10}
+// 	node2 := &Node{data: 20}
+// 	node3 := &Node{data: 30}
+
+// 	node1.next = node2
+// 	node2.next = node3
+
+// 	head := node1
+// 	current := head
+// 	target:=20
+
+// 	for current != nil {
+// 		if current.data==target{
+// 			fmt.Println("value found",current.data)
+// 			return
+// 		}
+// 		current=current.next
+// 	}
+// 	fmt.Println("value not found")
+
+// }
 
 // package main
 // import "fmt"
