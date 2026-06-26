dbef5e266b4d753d21b99f05971fce10982efe3e
Shabinn-k <shhabin.07@gmail.com>
2026-06-26 10:50:40 +0530

    practice

--- Day3/main.go
+++ Day3/main.go
@@ -36,39 +36,6 @@
 // }
 
 //INSERT AT BEGINNING
-// package main
-// import f "fmt"
-// type Node struct{
-// 	data int
-// 	next *Node
-// }
-// func main() {
-// 	node2:=&Node{data: 2}
-// 	node3:=&Node{data: 3}
-// 	node4:=&Node{data: 4}
-
-// 	node2.next=node3
-// 	node3.next=node4
-
-// 	head:=node2
-
-// 	node1:=&Node{data: 1}
-// 	node1.next=head
-// 	head=node1
-
-// 	node0:=&Node{data: 0}
-// 	node0.next=head
-// 	head=node0
-
-// 	current:=head
-
-// 	for current!=nil{
-// 		f.Println(current.data)
-// 		current=current.next
-// 	}
-// }
-
-//DELETING AT END
 package main
 import f "fmt"
 type Node struct{
@@ -76,26 +43,59 @@ type Node struct{
 	next *Node
 }
 func main() {
-	node1:=&Node{data: 1}
 	node2:=&Node{data: 2}
 	node3:=&Node{data: 3}
+	node4:=&Node{data: 4}
 
-	node1.next=node2
 	node2.next=node3
+	node3.next=node4
 
-	head:=node1
+	head:=node2
+
+	node1:=&Node{data: 1}
+	node1.next=head
+	head=node1
+
+	node0:=&Node{data: 0}
+	node0.next=head
+	head=node0
+
 	current:=head
 
-	for current.next.next!=nil{
+	for current!=nil{
+		f.Println(current.data)
 		current=current.next
 	}
-	current.next=nil
-
-	for head!=nil{
-		f.Println(head.data)
-		head=head.next
-	}
 }
+
+//DELETING AT END
+// package main
+// import f "fmt"
+// type Node struct{
+// 	data int
+// 	next *Node
+// }
+// func main() {
+// 	node1:=&Node{data: 1}
+// 	node2:=&Node{data: 2}
+// 	node3:=&Node{data: 3}
+
+// 	node1.next=node2
+// 	node2.next=node3
+
+// 	head:=node1
+// 	current:=head
+
+// 	for current.next.next!=nil{
+// 		current=current.next
+// 	}
+// 	current.next=nil
+
+// 	for head!=nil{
+// 		f.Println(head.data)
+// 		head=head.next
+// 	}
+// }
 
 //DELETING AT BEGINNING
 // package main
